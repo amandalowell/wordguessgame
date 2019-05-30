7e4d5eb5faa7da6a3d436988eee56c675301d7d3
Amanda Lowell <amandalowell2016@gmail.com>
2019-05-30 14:31:49 -0500

    tutor

--- game.js
+++ game.js
@@ -2,11 +2,30 @@ document.getElementById("box")
 
 
 
-var quizWords = [ "Revelations", "Kino", "Perkaholic", "Crawler", "Deadshot", "Jug"];
+var quizWords = [ "revelations", "kino", "perkaholic", "crawler", "deadshot", "jug", "boss", "revive"];
+var lettersUsed = new array ();
+var turnsLeft = 5;
 
 //this function selects the quizzword from the array//
 
 function getWord(){
     for (var i = 0; i < quizWords.length; i++) 
         console.log(quizWords[i]);
+}
+
+//make key-up, create letters already used
+
+document.onkeyup = function(event) {
+    var keyPressed = event.key;
+if (lettersUsed.indexOf(key) >= 0) {
+
+}
+
+else if(hadk) {
+
+}
+
+else  { }
+
+
 }
