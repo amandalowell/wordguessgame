d285706f5fdde9dbcb732706f14e08b4c4880982
Amanda Lowell <amandalowell2016@gmail.com>
2019-05-30 14:13:11 -0500

    java

---
+++ game.js
@@ -0,0 +1,12 @@
+document.getElementById("box")
+
+
+
+var quizWords = [ "Revelations", "Kino", "Perkaholic", "Crawler", "Deadshot", "Jug"];
+
+//this function selects the quizzword from the array//
+
+function getWord(){
+    for (var i = 0; i < quizWords.length; i++) 
+        console.log(quizWords[i]);
+}
